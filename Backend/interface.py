74ce071d36bfc0b34073a1822d75bbe342bb46a7
asubichev <asubichev@gmail.com>
2024-03-23 21:55:51 -0500

    interface methods init

--- Backend/interface.py
+++ Backend/interface.py
@@ -4,7 +4,7 @@
 # 'pip install flask-cors'
 
 # to start it up you do 
-# 'python test.py'
+# 'python <filename>.py'
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -17,6 +17,19 @@ def run_python_code():
     # Execute the Python code
     result = "Python received: [" + (code) + ']'
     return jsonify({'result': result})
+
+@app.route('/lccde', methods=['POST'])
+def alg1():
+    pass
+
+@app.route('/mth', methods=['POST'])
+def alg2():
+    pass
+
+@app.route('/tree-based', methods=['POST'])
+def alg3():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
